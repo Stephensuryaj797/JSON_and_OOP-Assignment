23c6066df0b75369bab2ed172a9c72fbb55e21f9
Stephen Surya J <146203803+Stephensuryaj797@users.noreply.github.com>
2023-11-14 21:47:13 +0530

    Update create_json.py

--- create_json.py
+++ create_json.py
@@ -1,11 +1,42 @@
 import json
-employee_info=[{"Name":"anbu","DOB":"16/01/1997","Height":"5'10","City":"selam","State":"Tamil nadu"},
-               {"Name":"Srinivas reddy","DOB":"24/04/1996","Height":"5'6","City":"Vijayawada","State":"Telengana"},
-               {"Name":"Renuka","DOB":"08/08/1997","Height":"5'4","City":"puna","State":"Maharastra"},
-               {"Name":"Abhijeet","DOB":"18/07/1999","Height":"5'3","City":"Varanasi","State":"Uttar Pradesh"},
-               {"Name":"Suman","DOB":"28/03/2001","Height":"5'4","City":"Ahmedabad","State":"Gujarat"}]
-with open("employee.json","w") as f:
-    d=json.dump(employee_info,f)
-with open("employee.json","r") as f:
-    x=json.load(f)
-    print(x)
+
+class Employee:
+    def __init__(self, name, dob, height, city, state):
+        self.name = name
+        self.dob = dob
+        self.height = height
+        self.city = city
+        self.state = state
+
+    def __repr__(self):
+        return f"{self.name}, {self.dob}, {self.height}, {self.city}, {self.state}"
+
+
+employee_data = [
+    {"name": "Dinesh", "dob": "04/04/2001", "height": "6'0", "city": "Chittoor", "state": "AP"},
+    {"name": "Bhuvan", "dob": "02/02/2000", "height": "5'6", "city": "Tirupati", "state": "AP"},
+    {"name": "Chakri", "dob": "03/03/2000", "height": "6'2", "city": "Vellore", "state": "TN"},
+    {"name": "Harish", "dob": "04/04/2000", "height": "5'5", "city": "Chennai", "state": "TN"},
+    {"name": "Bobby", "dob": "05/05/2000", "height": "6'1", "city": "Chittoor", "state": "AP"},
+]
+
+with open("employee.json", "w") as file:
+    json.dump(employee_data, file)
+
+# read the employee information from the JSON file
+employee_objects = []
+with open("employee.json", "r") as file:
+    employee_data = json.load(file)
+    for employee in employee_data:
+        employee_objects.append(
+            Employee(
+                name=employee["name"],
+                dob=employee["dob"],
+                height=employee["height"],
+                city=employee["city"],
+                state=employee["state"],
+            )
+        )
+
+# print the list of employee objects
+print(employee_objects)
